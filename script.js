ce168e9e8c5baf710c57871ed3f2c58d0da0a6f6
Rafael Days <130679216+Rafael-Days@users.noreply.github.com>
2024-04-04 23:32:18 +0000

    04/04

---
+++ script.js
@@ -0,0 +1,6 @@
+const params = new URLSearchParams(queryString);
+const q = parseInt.get("wartortle");
+
+const titulo = document.title.PokemonEsp;
+
+fetch("https://pokeapi.co/api/v2/pokemon/wartortle");
