43151cd8fbfde5acabaed885aa26acb93488c88e
Rafael Days <130679216+Rafael-Days@users.noreply.github.com>
2024-04-05 15:54:31 +0000

    script

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const h1 = document.querySelector('h1')
-h1.style.color = 'red'
+const h1T = document.querySelector('h1')
+//h1.style.color = 'red'
+h1T.textContent = 'Wartortle'
 
+let params = new URLSearchParams(queryString);
+let nameParam = parseInt(params.get("name")); 
+
+window.alert(nameParam);
+
+document.querySelector('h1').remove()
+///////////////////////
+/*
+const h1T = document.createElement('h1')
+
+h1T.textContent = 'Wartortle'
+*/
+
+////////////////////////////////////
 let queryString = window.location.search;
 
 if (queryString.hasOwnProperty('wartortle')) {
